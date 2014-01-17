ed57bdae4d54482cbb209b819a9ac2928ecbad18
Douglas Lovell <doug@wbreeze.com>
2014-01-16 21:36:55 -0500

    Initial Commit

---
+++ OAuthClientSetup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OAuthClientSetup
+//
+//  Created by Douglas Lovell on 1/16/14.
+//  Copyright (c) 2014 Telegraphy Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OACSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OACSAppDelegate class]));
+    }
+}
