92e9a28b3c964a61308bb95d568c77f0cc98797e
Douglas Lovell <doug@wbreeze.com>
2014-01-17 19:22:10 -0500

    add sample configuration

--- OAuthClientSetup/OACSConnectViewController.m
+++ OAuthClientSetup/OACSConnectViewController.m
@@ -18,7 +18,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.isConfigured = YES;
+        self.isConfigured = NO;
         self.isAuthenticated = NO;
     }
     return self;
