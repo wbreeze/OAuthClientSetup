f96c5994feff9f59db34a25876fcf9904244d530
Douglas Lovell <doug@wbreeze.com>
2014-01-18 09:50:26 -0500

    add window assertion to test connect window

--- integration/javascript/iphone/authorize_and_reset.js
+++ integration/javascript/iphone/authorize_and_reset.js
@@ -1,6 +1,19 @@
 #import "../../../../Pods/tuneup_js/tuneup.js"
 
+function onConnectView() {
+  assertWindow({
+      scrollViews: [{
+          staticTexts: [
+            { label: "Email" },
+            { label: "Password" }
+          ],
+          buttons: [ { name: "Connect" } ]
+      }]
+    });
+}
+
 test("authorize and reset", function(target, app) {
+     onConnectView();
 
      app.mainWindow().scrollViews()[0].textFields()[0].tap();
      app.keyboard().typeString("user@example.com");
