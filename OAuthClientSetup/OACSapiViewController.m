bd6e9772ca66ddba1ae8bb2e97b06d6889caa9f9
Douglas Lovell <doug@wbreeze.com>
2014-02-05 17:33:17 -0500

    initial implementation of request does not refresh token

--- OAuthClientSetup/OACSapiViewController.m
+++ OAuthClientSetup/OACSapiViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "OACSapiViewController.h"
+#import "OACSAppDelegate.h"
+#import "AFHTTPRequestOperation.h"
 
 @interface OACSapiViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *meButton;
@@ -36,6 +38,37 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sendMeRequest
+{
+    [self.meButton setEnabled:NO];
+    [self.workinOnIt startAnimating];
+    OACSAppDelegate *app = (OACSAppDelegate *)([UIApplication sharedApplication].delegate);
+    AFHTTPClient *client = app.httpClient;
+    [client setDefaultHeader:@"Authorization" value:[NSString stringWithFormat:@"Bearer %@", [app.creds accessToken]]];
+    [client getPath:@"api/v1/me.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Response has %@", responseObject);
+        [self.workinOnIt stopAnimating];
+        [self.meButton setEnabled:YES];
+        [self.resultText setText:[(NSObject *)responseObject description]];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"me request failure is %@", error);
+        NSHTTPURLResponse *response = operation.response;
+        [self.workinOnIt stopAnimating];
+        [self.meButton setEnabled:YES];
+        NSString *resultText  = [error localizedDescription];
+        if (response.statusCode == 401) {
+            resultText = @"Failed to authorize";
+        }
+        [self.resultText setText:resultText];
+    }];
+}
+
+- (IBAction)sendProfilesRequest
+{
+    [self.profilesButton setEnabled:NO];
+    [self.workinOnIt startAnimating];
 }
 
 @end
